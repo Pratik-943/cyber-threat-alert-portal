4bbb2d0b134d6407cacfcfaca377522d4d56421f
pratik <pratik@PRATIK>
2026-01-24 23:40:25 +0530

    Project Start

---
+++ src/main/java/com/project/cyberalert/CyberThreatAlertPortalApplication.java
@@ -0,0 +1,13 @@
+package com.project.cyberalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberThreatAlertPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberThreatAlertPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cyberalert/CyberThreatAlertPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cyberalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberThreatAlertPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
